cac1ca6de6d9997c59635a1855f685e028d26eae
smcgdub <stephen_xyz1@hotmail.com>
2021-07-07 21:51:56 +0000

    Last save of the night

---
+++ todo/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+class TestDjango(TestCase):
+
+    def test_this_works1(self):
+        self.assertEqual(1, 1)
+
+    # def test_this_works2(self):
+    #     self.assertEqual(1, 1)
+
+    # def test_this_works3(self):
+    #     self.assertEqual(1, 1)
+
+    # def test_this_works4(self):
+    #     self.assertEqual(1, 1)
